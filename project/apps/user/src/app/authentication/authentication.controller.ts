91e9e86956f56d56904aff512c4f4ae35f4a7ced
Rinat <r.z.zaripov@gmail.com>
2023-12-07 15:53:37 +0300

    добавил теги, описания и статусы для swagger

--- project/apps/user/src/app/authentication/authentication.controller.ts
+++ project/apps/user/src/app/authentication/authentication.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, HttpStatus } from '@nestjs/common';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
 import { AuthenticationService } from './authentication.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { fillDTO } from '@project/libs/shared/helpers';
@@ -6,28 +7,46 @@ import { UserRdo } from './rdo/user.rdo';
 import { LoginUserDto } from './dto/login-user.dto';
 import { LoggedUserRdo } from './rdo/logged-user.rdo';
 
+@ApiTags('authentication')
 @Controller('auth')
 export class AuthenticationController {
   constructor(
     private readonly authService: AuthenticationService
   ) {}
 
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The new user has been created.'
+  })
   @Post('signin')
   public async create(@Body() dto: CreateUserDto): Promise<UserRdo | null> {
     const newUser = await this.authService.registerNewUser(dto);
     return fillDTO(UserRdo, newUser.toPOJO());
   }
 
+  @ApiResponse({
+    type: LoggedUserRdo,
+    status: HttpStatus.OK,
+    description: 'User has been successfully logged in.'
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Password of login is wrong.'
+  })
   @Post('login')
   public async login(@Body() dto: LoginUserDto): Promise<LoggedUserRdo | null> {
     const verifiedUser = await this.authService.verifyUser(dto);
     return fillDTO(LoggedUserRdo, verifiedUser);
   }
 
+  @ApiResponse({
+    type: UserRdo,
+    status: HttpStatus.OK,
+    description: 'User found'
+  })
   @Get(':id')
   public async show(@Param('id') userId: string): Promise<UserRdo | null> {
     const existUser = await this.authService.getUserEntity(userId);
     return fillDTO(UserRdo, existUser.toPOJO());
   }
-
 }
--- project/apps/user/src/app/authentication/rdo/user.rdo.ts
+++ project/apps/user/src/app/authentication/rdo/user.rdo.ts
@@ -1,22 +1,39 @@
 import { Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UserRdo {
   @Expose()
+  @ApiProperty({
+    description: 'User unique ID',
+    example: '1234-5678-9012-3456'
+  })
   public id: string;
 
   @Expose()
+  @ApiProperty({
+    description: 'User unique email address',
+    example: 'user@user.com'
+  })
   public email: string;
 
   @Expose()
+  @ApiProperty({
+    description: 'User first name',
+    example: 'Alex'
+  })
   public firstname: string;
 
   @Expose()
+  @ApiProperty({
+    description: 'User last name',
+    example: 'Bochkin'
+  })
   public lastname: string;
 
   @Expose()
+  @ApiProperty({
+    description: 'User avatar path',
+    example: '/images/user.png'
+  })
   public avatar: string;
-
-  @Expose()
-  public accessToken: string;
-
 }

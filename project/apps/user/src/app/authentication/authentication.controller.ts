448c91274ebbbc43cf3d81b993467b44034a56f4
Rinat <r.z.zaripov@gmail.com>
2023-12-07 15:03:55 +0300

    Добавил в контроллер authentication методы регистрации нового пользователя, логина и получения данных о пользователе по id

--- project/apps/user/src/app/authentication/authentication.controller.ts
+++ project/apps/user/src/app/authentication/authentication.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { fillDTO } from '@project/libs/shared/helpers';
+import { UserRdo } from './rdo/user.rdo';
+import { LoginUserDto } from './dto/login-user.dto';
+import { LoggedUserRdo } from './rdo/logged-user.rdo';
 
-@Controller('authentication')
-export class AuthenticationController {}
+@Controller('auth')
+export class AuthenticationController {
+  constructor(
+    private readonly authService: AuthenticationService
+  ) {}
+
+  @Post('signin')
+  public async create(@Body() dto: CreateUserDto): Promise<UserRdo | null> {
+    const newUser = await this.authService.registerNewUser(dto);
+    return fillDTO(UserRdo, newUser.toPOJO());
+  }
+
+  @Post('login')
+  public async login(@Body() dto: LoginUserDto): Promise<LoggedUserRdo | null> {
+    const verifiedUser = await this.authService.verifyUser(dto);
+    return fillDTO(LoggedUserRdo, verifiedUser);
+  }
+
+  @Get(':id')
+  public async show(@Param('id') userId: string): Promise<UserRdo | null> {
+    const existUser = await this.authService.getUserEntity(userId);
+    return fillDTO(UserRdo, existUser.toPOJO());
+  }
+
+}

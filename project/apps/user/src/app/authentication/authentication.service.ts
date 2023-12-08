87d960c7fe433a702e9c0dc61d76892685d6bcee
Rinat <r.z.zaripov@gmail.com>
2023-12-08 13:34:20 +0300

    исправления в нейминге user

--- project/apps/user/src/app/authentication/authentication.service.ts
+++ project/apps/user/src/app/authentication/authentication.service.ts
@@ -4,18 +4,17 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { AUTH_USER_EXISTS, AUTH_USER_NOT_FOUND, AUTH_USER_PASSWORD_WRONG } from './authentication.constant';
 import { UserEntity } from '../user/user.entity';
 import { LoginUserDto } from './dto/login-user.dto';
-import { UserRdo } from './rdo/user.rdo';
 
 @Injectable()
 export class AuthenticationService {
   constructor(
-    private readonly UserRepository: UserRepository
+    private readonly userRepository: UserRepository
   ) {}
 
   public async registerNewUser(dto: CreateUserDto): Promise<UserEntity> {
     const {email, firstname, lastname, password} = dto;
 
-    const User = {
+    const user = {
       email,
       firstname,
       lastname,
@@ -23,34 +22,34 @@ export class AuthenticationService {
       avatar: ''
     }
 
-    const existUser = await this.UserRepository.findByEmail(email);
+    const existUser = await this.userRepository.findByEmail(email);
 
     if(existUser) {
       throw new ConflictException(AUTH_USER_EXISTS);
     }
 
-    const userEntity = await new UserEntity(User).setPassword(password);
+    const userEntity = await new UserEntity(user).setPassword(password);
 
-    return this.UserRepository.save(userEntity);
+    return this.userRepository.save(userEntity);
   }
 
   public async verifyUser(dto: LoginUserDto): Promise<UserEntity> {
     const {email, password} = dto;
-    const existUser = await this.UserRepository.findByEmail(email);
+    const existUser = await this.userRepository.findByEmail(email);
 
     if (!existUser) {
       throw new NotFoundException(AUTH_USER_NOT_FOUND);
     }
 
-    const UserEntity = new UserEntity(existUser);
-    if (!(await UserEntity.comparePassword(password))) {
+    const userEntity = new UserEntity(existUser);
+    if (!(await userEntity.comparePassword(password))) {
       throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
     }
 
-    return UserEntity
+    return userEntity
   }
 
   public async getUserEntity(id: string): Promise<UserEntity> {
-    return this.UserRepository.findById(id);
+    return this.userRepository.findById(id);
   }
 }
--- project/apps/user/src/app/user/user.repository.ts
+++ project/apps/user/src/app/user/user.repository.ts
@@ -1,6 +1,6 @@
 import { BaseMemoryRepository } from '@project/libs/shared/core';
-import { UserEntity } from './user.entity';
 import { Injectable } from '@nestjs/common';
+import { UserEntity } from './user.entity';
 
 @Injectable()
 export class UserRepository extends BaseMemoryRepository<UserEntity> {

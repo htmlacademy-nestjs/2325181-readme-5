900f7ca6cb72f092199b33f38c882c44fa4930c7
Rinat <r.z.zaripov@gmail.com>
2023-12-25 14:02:25 +0300

    обязательные поля в BasePost interface

--- project/libs/shared/app/types/src/lib/post/base-post.interface.ts
+++ project/libs/shared/app/types/src/lib/post/base-post.interface.ts
@@ -1,15 +1,15 @@
 import { PostTypeValues } from './post-type.type';
+import { Entity, EntityIdType} from '@project/libs/shared/core'
 
-export interface BasePost{
+export interface BasePost extends Entity<EntityIdType> {
   id?: string;
-  type?: PostTypeValues;
-  tags?: string[];
-  authorId?: string;
-  isPublished?: boolean;
-  isRepost?: boolean;
-  originPostId?: string;
-  originAuthorId?: string;
+  type: PostTypeValues;
+  tags: string[];
+  authorId: string;
+  isPublished: boolean;
+  isRepost: boolean;
+  originPostId: string;
+  originAuthorId: string;
   createdAt?: Date;
   publishedAt?: Date;
-
 }

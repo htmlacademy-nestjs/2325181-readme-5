1d2369fcaabe0e0a4dc3e8f5fd17173c1586a7c7
Rinat <r.z.zaripov@gmail.com>
2024-01-10 13:56:58 +0300

    feat(user): подключил глобальный ValidationPipe

--- project/apps/user/src/main.ts
+++ project/apps/user/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
@@ -21,6 +21,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('spec', app, document);
+  app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   const port = configService.get('application.port');
   await app.listen(port);

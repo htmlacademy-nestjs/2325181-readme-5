4c442c2e3597a0464645866804f6986ade5a7e84
Rinat <r.z.zaripov@gmail.com>
2024-01-10 13:56:33 +0300

    feat(post): подключил PrismaClientModule

--- project/apps/post/src/app/post/post.module.ts
+++ project/apps/post/src/app/post/post.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PostRepository } from './post.repository';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { PrismaClientModule } from '@project/libs/shared/post/models';
 
 
 @Module({
+  imports: [PrismaClientModule],
   providers: [PostRepository, PostService],
   controllers: [ PostController],
   exports: [PostRepository, PostService]

793d098c65ba0340e5b09303c5af779a8946d5d2
Rinat <r.z.zaripov@gmail.com>
2024-01-21 07:25:28 +0300

    fix(localStrategy): исправил импорт

--- project/apps/user/src/app/authentication/strategies/local.strategy.ts
+++ project/apps/user/src/app/authentication/strategies/local.strategy.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { Strategy } from 'passport-local';
 import { AuthenticationService } from '../authentication.service';
 import { USERNAME_FIELD } from '../authentication.constant';
 import { AuthUser } from '@project/libs/shared/app/types';
